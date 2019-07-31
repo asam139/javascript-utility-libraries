f702b9cab699f9fd1db73b6a3c68663430532bae
joaquin caceres galan <jcg230795@gmail.com>
2019-07-31 20:20:57 +0200

    Solution example 02_calling.js

--- examples/02_calling.js
+++ examples/02_calling.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const L = require('lodash');
 
 let res = R.call(R.add, 1, 2);
 console.log(res);
@@ -6,3 +7,13 @@ console.log(res);
 console.log(R.call(R.repeat, 'x')(5));
 
 R.call(console.log, [1, 2, 3]);
+
+console.log("--------------------------");
+
+//Solutions
+rest2 = L.add(1,2)
+console.log(rest2);
+
+console.log(L.times(5,L.constant( 'x' ) ) )
+L.times(1,console.log([1,2,3]));
+
