b52bb1b1a4e6607436d99ca53b23205c7530275e
joaquin caceres galan <jcg230795@gmail.com>
2019-07-31 20:32:37 +0200

    Solution 06_length.js

--- examples/06_length.js
+++ examples/06_length.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const L = require('lodash');
 
 let nums = [1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6, 7];
 
@@ -7,3 +8,12 @@ console.log(n1);
 
 let n2 = R.length(R.uniq(nums));
 console.log(n2);
+
+
+//-----------solutions--------------------------
+console.log("-------------------------------")
+let n3 = L.size(nums);
+console.log(n3);
+
+let n4 = L.size(L.uniqWith(nums, L.isEqual) );
+console.log(n4);
