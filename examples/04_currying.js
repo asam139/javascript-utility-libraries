ed923ce5009a677261d8de2323cfff9e50b76700
JaimeGuarino <jakueguarino@gmail.com>
2019-08-06 21:59:32 +0200

    currying Lodash.

--- examples/04_currying.js
+++ examples/04_currying.js
@@ -1,6 +1,18 @@
 const R = require('ramda');
+const _ = require('lodash');
+
 
 let addOneToAll = R.map(R.add(1));
 let res = addOneToAll([1,2,3]);
 
 console.log(res);
+let array = [1, 2, 3];
+
+console.log('/////////////////');
+
+res1 = _.map(array, function (num){ return num + 1});
+
+console.log(res1);
+
+
+
