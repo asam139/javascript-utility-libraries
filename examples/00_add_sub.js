b8c9c4b923cf6301398a614a70cafbc0c4464dd7
JaimeGuarino <jakueguarino@gmail.com>
2019-08-06 21:28:35 +0200

    Add and Sub ramda to lodash. Easy :)

--- examples/00_add_sub.js
+++ examples/00_add_sub.js
@@ -1,7 +1,15 @@
 const R = require('ramda');
+const _ = require('lodash')
 
 console.log(R.add(2, 5));
 console.log(R.subtract(2, 5));
 
 let res = R.add(R.add(2, 5), R.subtract(2, 10));
 console.log(res);
+console.log('//////////////////////////'); 
+
+console.log(_.add(2, 5));
+console.log(_.subtract(2, 5));
+
+let res1 = _.add(_.add(2, 5), _.subtract(2, 10));
+console.log(res1);
