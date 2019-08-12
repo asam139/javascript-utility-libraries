d8ad27f7d1211ecdffaad317341a912dcca1c5e0
JoaquinCG <jcg230795@gmail.com>
2019-08-12 19:11:57 +0200

    18_filtering solution

--- examples/18_filtering.js
+++ examples/18_filtering.js
@@ -1,5 +1,5 @@
 const R = require('ramda');
-
+const L = require('lodash');
 const nums = [-3, -1, 0, 2, 3, 4, 5, 6, 7]
 
 let res = R.filter(x => x > 0, nums);
@@ -12,3 +12,14 @@ const isEven = x => x % 2 === 0;
 
 let filtered = R.filter(isEven, nums);
 console.log(filtered);
+
+
+
+//----------------------solution---------------------------------------------------
+console.log("---------------------------------------------------")
+
+let res3 = L.filter(nums,x => x > 0);
+console.log(res3);
+
+let res4 = L.filter(nums,x => x < 0);
+console.log(res4);
