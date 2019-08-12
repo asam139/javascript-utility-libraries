d624fa57cce0d8c7b6cc1e0c8d4771f9cfd20cf2
JoaquinCG <jcg230795@gmail.com>
2019-08-12 19:16:10 +0200

    22_reduce solution

--- examples/22_reduce.js
+++ examples/22_reduce.js
@@ -1,5 +1,5 @@
 const R = require('ramda');
-
+const L = require('lodash');
 let nums = [2, 3, 4, 5, 6, 7];
 
 let sum = R.reduce((x, y) => x+y, 0, nums);
@@ -7,3 +7,11 @@ console.log(sum);
 
 let product = R.reduce((x, y) => x*y, 1, nums);
 console.log(product);
+
+
+//----------------------solution---------------------------------------------------
+console.log("---------------------------------------------------")
+
+
+let sum2 = L.reduce(nums,(x, y) => x+y, 0);
+console.log(sum2);
